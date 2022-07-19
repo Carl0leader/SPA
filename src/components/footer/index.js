3e3c9afccb2a47236c04f548715c6cdad8ea6d77
Giancarlo De Luca <carloleader@gmail.com>
2022-07-19 10:15:53 +0200

    update all file Single Page Application

---
+++ src/components/footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer(attr) {
+    return (
+      <div className="footer-component">
+        <footer className="mastfoot mt-auto">
+          <div className="inner">
+            <p>Corso Front End, by <strong>Tutti i corsisti!</strong></p>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+  
+  export default Footer;
+  
